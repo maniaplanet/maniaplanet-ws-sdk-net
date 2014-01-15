1386d7e69973884c9dc9dbae1b210cfd5f898ac1
satanasdiabolo <philippe@melot.eu>
2014-01-15 09:28:58 +0100

    Update class to be compatible with SDK v 5.0.1

--- ManiaFlash/Message.cs
+++ ManiaFlash/Message.cs
@@ -12,9 +12,10 @@ namespace ManiaPlanetWSSDK.ManiaFlash
 		public string author { get; set; }
 		public string dateCreated { get; set; }
 		public string message { get; set; }
+		public string longMessage { get; set; }
 		public string link { get; set; }
 		public string iconStyle { get; set; }
 		public string iconSubStyle { get; set; }
-		public object mediaURL { get; set; }
+		public string mediaURL { get; set; }
 	}
 }
